540d9b886ce987eee60eb5d9165d1964c921b9a9
ZavgorodnyD <146486151+ZavgorodnyD@users.noreply.github.com>
2024-07-07 12:58:29 +0300

    next commit

---
+++ aspDemoProject/aspDemoProject/Controllers/POSTController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspDemoProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class POSTController : ControllerBase
+    {
+        //POST api/postexample
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return Ok($"received: {value}");
+        }
+
+        //POST api/postexample/data
+        [HttpPost("data")]
+        public ActionResult<object> PostData([FromBody] DataModel data)
+        {
+            return Ok(data);
+        }
+    }
+}
---
+++ aspDemoProject/aspDemoProject/DataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace aspDemoProject
+{
+    public class DataModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
